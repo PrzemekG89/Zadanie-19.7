242fc91c8597fe3b9e600dfec9d33a275514a6fc
Przemek Gałdys <galdys.przemek@gmail.com>
2018-01-15 19:39:04 +0100

    Logger and DevTools

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+//import { createStore, applyMiddleware } from 'redux';
+//import { createLogger} from 'redux-logger';
+import { createStore, combineReducers } from 'redux';
+import DevTools from './DevTools';
 import reducer from './reducer.js';
 import { addComment } from './actions.js';
 
+//const logger = createLogger();
+//const store = createStore(
+//    reducer,
+//    applyMiddleware(logger)
+//);
 
-
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    DevTools.instrument()
+);
 
 ReactDOM.render(
     <Provider store={store}>
